47f5930ebde5e47ed5feac6d390f0a41c55df68b
Vaibhav Kumar <urstrulyvaibhav026@gmail.com>
2023-02-18 22:31:55 +0530

    Initial Commit

--- force-app/main/default/lwc/toDoManager/toDoManager.js
+++ force-app/main/default/lwc/toDoManager/toDoManager.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class ToDoManager extends LightningElement {}
+export default class ToDoManager extends LightningElement {
+
+    time = "5:35 PM";
+    greetings = "Good Day"
+}
